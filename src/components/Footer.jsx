a222b94b5016e0616c47ab89416af7519a799286
faizkhan-06 <157209968+faizkhan-06@users.noreply.github.com>
2024-10-04 17:29:59 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="text-black-100 text-center text-xl pt-4 font-medium font-Kalam">
-      <p>Created by Faiz</p>
+      <p>
+        Created by{" "}
+        <a
+          href="https://github.com/faizkhan-06"
+          target="_blank"
+          className=" text-blue-700"
+        >
+          Faiz
+        </a>
+      </p>
     </footer>
   );
 };
